cdc4a89fbd4565b32b7cb230b74bcd20c5ec881c
rafayshahid6117 <mshahidzaman@gmail.com>
2025-07-12 10:58:29 +0500

    rafay

---
+++ lesson20/demo.js
@@ -0,0 +1,33 @@
+function canihavelunch(){
+    console.log("aoa! mom can i have lunch?");
+    console.log("please let me have lunch i am very hungry");
+}
+
+canihavelunch()
+canihavelunch()
+canihavelunch()
+canihavelunch()
+canihavelunch()
+canihavelunch()  
+canihavelunch()
+
+function sum(a,b,c){
+    var result = a + b + c;
+    console.log(result)
+}
+
+console.log("refrensing the local variable of function", result)
+
+sum(5, 67, 89)
+sum(10, 20, 30)
+
+
+function multiply(a, b, c, d){
+    var result = a * b * c * d;
+    console.log(result)
+}
+
+var k = multiply(2.55654, 3.456, 4.567, 5.678);
+console.log(k)
+
+
