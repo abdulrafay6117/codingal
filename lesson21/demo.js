3ea6941256660e7938fae97149ace991971af407
rafayshahid6117 <mshahidzaman@gmail.com>
2025-07-13 11:02:21 +0500

    rafay

---
+++ lesson21/demo.js
@@ -0,0 +1,35 @@
+var a = Math.random()
+console.log(a)
+
+var b = {x:98,saybye:function() {return "allah hafiz is bye for muslims"}}
+
+var c = b.saybye()
+console.log(c)
+
+var d= "rafay"
+console.log(d.toupperCase())    
+console.log(d.tolowerCase())
+
+function bye(a){
+    console.log(a)
+}
+
+let i = 0
+
+for(i=0; i < 10; i++){
+    console.log(i)
+}
+
+var day = 2
+
+switch(day){
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+}
