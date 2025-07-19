674a76a03d5fe8c63281b1f8ba4a994166893418
rafayshahid6117 <mshahidzaman@gmail.com>
2025-07-19 10:56:34 +0500

    rafay

---
+++ lesson22/demo.js
@@ -0,0 +1,18 @@
+var a = {pencil: "pencil is used for writing", eraser:"eraser is used for erasing", sharpener:"sharpener is used for sharpening", ruler:"ruler is used for measuring",  notebook:"notebook is used for writing notes", addition: 99+140, subtraction: 45-100}   
+
+console.log(a.pencil) 
+console.log(a.eraser)
+console.log(a.sharpener)
+console.log(a.ruler)
+console.log(a.notebook)
+console.log(a.addition())
+console.log(a.subtraction())
+
+console.log(MATHS.random())
+console.log(MATHS>PI)
+
+var b ="base ball and base ball bat"
+console.log(b.toUpperCase())
+
+var c = 2
+console.log(c.toExponential())
