6445460ef44da4e86ac6e5adae915ba4fc5bd590
rafayshahid6117 <mshahidzaman@gmail.com>
2025-07-26 10:55:58 +0500

    rafay

---
+++ lesson 24/script.js
@@ -0,0 +1,5 @@
+document.getElementsByClassName('rafayclass')[0].style.backgroundColor = 'grey';
+
+document.getElementsByClassName('hulk')[0].innerHTML = 'HULK SMASH!';
+
+document.getElementsByTagName('h4')[0].innerHTML = 'iron man will kill everyone!';
