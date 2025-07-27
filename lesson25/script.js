72e25f7dd43e6e17dacd6eea0200e7e5c2e4b251
rafayshahid6117 <mshahidzaman@gmail.com>
2025-07-27 11:13:06 +0500

    rafay

---
+++ lesson25/script.js
@@ -0,0 +1,37 @@
+var a = 90
+console.log(typeof (a))
+var b = 'today is amazing weather'
+console.log(typeof (b))
+var c = true
+console.log(typeof (c))
+var d = {name: 'rafay', age: 14,}
+console.log(typeof (d))
+var e = [80, 90, 100, 110, 120, 130]
+console.log(typeof (e))
+var f = null
+var g = undefined
+
+if (a < 100) {
+    console.log('amazing i am at the top of the world')
+} else if (a > 90) {
+    console.log('its ok to be in the middle of the world')
+} else {
+    console.log('oh i am at the bottom of the world')
+}
+
+let i 
+for(i=0; i < 100; i++) {
+    console.log("pizza")
+}
+
+var w ='www.google.com'
+
+let k = 60000
+k = 60
+k = 'ahahahahahah'
+k = [50,60,70]
+
+const z = 'donkey'
+z = 'pizza'
+
+console.log(k)
