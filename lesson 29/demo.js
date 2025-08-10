46a1c795aed657ccf8a0c911b22bf7bc593daa0d
rafayshahid6117 <mshahidzaman@gmail.com>
2025-08-10 11:16:54 +0500

    rafay

---
+++ lesson 29/demo.js
@@ -0,0 +1,29 @@
+// json: javascript object notation
+
+// var a = {name: 'badmash', age: 20, isMale: true, hobbies: 'kill everyone'}
+
+// console.log(a);
+
+// var b = JSON.stringify(a);
+
+// console.log(b);
+
+// var c = JSON.parse(b);
+
+// console.log(c);
+
+
+console.log('startinggg')
+
+setTimeout(() => {
+    console.log('workdone hurray')
+}, 1000)
+
+console.log('endinggg')
+
+var p = new Promise((resolve, reject) => {
+    setTimeout(() => { resolve(" finally won") }, 7000)
+})
+
+p.then((data) => console.log(data)).catch((err) => console.log("opps we were not able to win: ", err))
+        
