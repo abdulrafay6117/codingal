59ad44214ba2f4f84d263270a40603deb1ed6c7c
rafayshahid6117 <mshahidzaman@gmail.com>
2025-09-28 11:05:03 +0500

    rafay

---
+++ python work/7/python.py
@@ -0,0 +1,22 @@
+abc = ['talha anjum', 'talha yunus', 'young stunners', 'bohemia', 'kr$na' ]
+
+print(abc[0])
+print(abc[1])
+print(abc[2])
+
+print(len(abc))
+
+print(abc [1:3])
+
+abc.append('rafay')
+print(abc)
+
+abc.pop()
+
+print(abc)
+
+rafay_list = ['4544', '54412', '895', '44124', '7825' ]
+
+rafay_list.sort()
+
+print (rafay_list)
